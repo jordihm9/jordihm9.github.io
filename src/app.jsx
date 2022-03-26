8bb444e9efa89c9dc86fa17249696f4e0616f7a8
Jordi Hernàndez i Magí <jjordihm9@gmail.com>
2022-03-26 18:20:10 +0100

    created main layout components

---
+++ src/app.jsx
@@ -0,0 +1,13 @@
+import { Header } from './components/header';
+import { Main } from './components/main';
+
+import './css/styles.css';
+
+export const App = () => {
+  return (
+    <>
+      <Header />
+      <Main />
+    </>
+  );
+};
